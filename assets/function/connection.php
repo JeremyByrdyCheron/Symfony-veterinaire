df271ab1f4f7101837cf94f1f67a0376e5ecc399
Nighthaunt25 <louis.ganeau2611@gmail.com>
2026-04-08 15:40:01 +0200

    connection

---
+++ assets/function/connection.php
@@ -0,0 +1,14 @@
+<?php
+include '../../.env';
+$host = 'db';
+$dbname = 'carnet_adresses';
+$username = 'root';
+$password = '*%*a!@poX3kMQ0'; // Mot de passe vide par défaut sous Laragon
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erreur de connexion à la base de données : " . $e->getMessage();
+    die();
+}
