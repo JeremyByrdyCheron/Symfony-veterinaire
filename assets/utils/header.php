5419b5709dab8a45975d154714a09eef19f773eb
ManonDufour8 <manon.dufour2608@gmail.com>
2026-04-08 15:16:57 +0200

    create header footer

---
+++ assets/utils/header.php
@@ -0,0 +1,20 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'accueil</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <img src="" alt="">
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="consultDocument.php">Accès au document</a></li>
+                <li><a href="formulaire.php">Prise de rendez-vous</a></li>
+            </ul>
+        </nav>
+
+    </header>
