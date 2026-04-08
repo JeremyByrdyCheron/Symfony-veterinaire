2c51ed5c9e9bb2c809ae621fef1ed780524154b7
ManonDufour8 <manon.dufour2608@gmail.com>
2026-04-08 23:03:31 +0200

    add form consultation document

---
+++ consultDocument.php
@@ -0,0 +1,15 @@
+<?php
+include 'assets/utils/header.php';
+ ?>
+
+<form action="" method="post">
+    <div>
+        <label for="code">Entrez votre code : </label>
+        <input type="text" name="code" id="code" required />
+    </div>
+    <button type="submit">Valider</button>
+</form>
+
+<?php 
+include 'assets/utils/footer.php';
+?>
