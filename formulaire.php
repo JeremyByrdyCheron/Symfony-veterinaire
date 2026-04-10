f34d5275f7ce8ce11a0ca45144a10db2151ea1b7
ManonDufour8 <manon.dufour2608@gmail.com>
2026-04-10 10:28:21 +0200

    add form css

--- formulaire.php
+++ formulaire.php
@@ -44,73 +44,78 @@ include "assets/utils/header.php";
     echo "<p style='color:green'>$success</p>"; ?>
 <?php if (isset($error))
     echo "<p style='color:red'>$error</p>"; ?>
-
-<form action="" method="post">
-    <div>
-        <label for="name">Nom : </label>
-        <input type="text" name="name" id="name" required />
+<section id="formulaire">
+    <div id="img_form">
+        <img src="assets/img/illustration_form.jpg" alt="Illustration chien et veterinaires">
     </div>
-    <div>
-        <label for="firstname">Prénom : </label>
-        <input type="text" name="firstname" id="firstname" required />
-    </div>
-    <div>
-        <label for="email">Adresse mail : </label>
-        <input type="email" name="email" id="email" required />
-    </div>
-    <div>
-        <label for="phone">Numéro de téléphone : </label>
-        <input type="tel" name="phone" id="phone" required />
-    </div>
-    <div>
-        <label for="pet">Votre animal : </label>
-        <select name="pet" id="pet" required>
-            <option value="">--Choisissez--</option>
-            <option value="dog">Chien</option>
-            <option value="cat">Chat</option>
-            <option value="rabbit">Lapin</option>
-            <option value="guineaPig">Cochon d'Inde</option>
-            <option value="hamster">Hamster</option>
-            <option value="bird">Oiseau</option>
-            <option value="Reptile">Reptile</option>
-            <option value="others">Autres</option>
-        </select>
-    </div>
-    <div>
-        <label for="reason">Motif de la visite : </label>
-        <select name="reason" id="reason" required>
-            <option value="">--Choisissez--</option>
-            <option value="emergency">Urgence</option>
-            <option value="vaccination">Vaccination</option>
-            <option value="sterilization">Stérilisation</option>
-            <option value="check">Bilan de santé</option>
-            <option value="identification">Identification</option>
-            <option value="dermatology">Dermatologie</option>
-            <option value="wound">Blessures</option>
-            <option value="other">Autres</option>
-        </select>
-    </div>
-    <div>
-        <label for="info">Dites nous en plus :</label>
-        <textarea id="info" name="info" rows="5" cols="33" placeholder="Informations complémentaires..."></textarea>
-    </div>
-    <div>
-        <label for="dateTime">Date et Heure : </label>
-        <input id="timeInput" type="time" min="09:00" max="18:00" step="3600">
-        <input type="datetime-local" id="dateTime" name="dateTime" required />
-    </div>
-    <button type="submit">Envoyer</button>
-    <script>
-        const input = document.getElementById("dateTime");
-        input.addEventListener("input", () => {
-            console.log()
-            if (input.value < "09:00") {
-                input.value = "09:00";
-            }else if(input.value > "18:00"){
-                input.value = "18:00"
-            }
-        });
-    </script>
-</form>
+    <form action="" method="post">
+        <h2>Prenez votre rendez-vous !</h2>
+        <div class="padd_form">
+            <label for="name">Nom : </label>
+            <input type="text" name="name" id="name" required />
+        </div>
+        <div class="padd_form">
+            <label for="firstname">Prénom : </label>
+            <input type="text" name="firstname" id="firstname" required />
+        </div>
+        <div class="padd_form">
+            <label for="email">Adresse mail : </label>
+            <input type="email" name="email" id="email" required />
+        </div>
+        <div class="padd_form">
+            <label for="phone">Numéro de téléphone : </label>
+            <input type="tel" name="phone" id="phone" required />
+        </div>
+        <div class="padd_form">
+            <label for="pet">Votre animal : </label>
+            <select name="pet" id="pet" required>
+                <option value="">--Choisissez--</option>
+                <option value="dog">Chien</option>
+                <option value="cat">Chat</option>
+                <option value="rabbit">Lapin</option>
+                <option value="guineaPig">Cochon d'Inde</option>
+                <option value="hamster">Hamster</option>
+                <option value="bird">Oiseau</option>
+                <option value="Reptile">Reptile</option>
+                <option value="others">Autres</option>
+            </select>
+        </div>
+        <div class="padd_form">
+            <label for="reason">Motif de la visite : </label>
+            <select name="reason" id="reason" required>
+                <option value="">--Choisissez--</option>
+                <option value="emergency">Urgence</option>
+                <option value="vaccination">Vaccination</option>
+                <option value="sterilization">Stérilisation</option>
+                <option value="check">Bilan de santé</option>
+                <option value="identification">Identification</option>
+                <option value="dermatology">Dermatologie</option>
+                <option value="wound">Blessures</option>
+                <option value="other">Autres</option>
+            </select>
+        </div>
+        <div class="padd_form">
+            <label for="info">Dites nous en plus :</label>
+            <textarea id="info" name="info" rows="3" cols="25" placeholder="Informations complémentaires..."></textarea>
+        </div>
+        <div class="padd_form">
+            <label for="dateTime">Date et Heure : </label>
+            <input id="timeInput" type="time" min="09:00" max="18:00" step="3600">
+            <input type="datetime-local" id="dateTime" name="dateTime" required />
+        </div>
+        <button type="submit">Envoyer</button>
+        <script>
+            const input = document.getElementById("dateTime");
+            input.addEventListener("input", () => {
+                console.log()
+                if (input.value < "09:00") {
+                    input.value = "09:00";
+                }else if(input.value > "18:00"){
+                    input.value = "18:00"
+                }
+            });
+        </script>
+    </form>
+</section>
 
 <?php include "assets/utils/footer.php"; ?>
