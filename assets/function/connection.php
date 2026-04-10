fa2ce4b3ba2e0157ba18c0404c047fdfd9166342
Nighthaunt25 <louis.ganeau2611@gmail.com>
2026-04-10 11:15:49 +0200

    ajout code

--- assets/function/connection.php
+++ assets/function/connection.php
@@ -1,11 +1,13 @@
 <?php
-$host = 'db';
-$dbname = 'carnet_adresses';
-$username = 'root';
-$password = '*%*a!@poX3kMQ0'; // Mot de passe vide par défaut sous Laragon
+
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$dbname = getenv('DB_NAME');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD'); // Mot de passe vide par défaut sous Laragon
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Erreur de connexion à la base de données : " . $e->getMessage();
